32cf5a63759fcedd37b9d886a12910538eb17573
Ali Roohi <Aliroohi1306@gmail.com>
2024-11-01 15:54:04 +0330

    completing

--- cloud_practice2/API_app/views.py
+++ cloud_practice2/API_app/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 import requests
 from django.http import HttpResponse
+from django.core.cache import cache
 
 def dictionary_definition(request,word):
-    print(word)
+
+    cached_data = cache.get(word)
+
+    if(cached_data):
+        return(HttpResponse(f"response is from cache: {cached_data}"))
+
+
 
     api_url = 'https://api.api-ninjas.com/v1/dictionary?word={}'.format(word)
     response = requests.get(api_url, headers={'X-Api-Key': 'UQNmRzC9t1DfaansEEfz1Q==04WUlN7tuwStTX9f'})
+
     if response.status_code == requests.codes.ok:
-        return HttpResponse(response.text)
+        cache.set(word,response.text,timeout=300)
+        return HttpResponse(f"response is from API: {response.text}")
     else:
         print("Error:", response.status_code, response.text)
 

2ed44bdec76f92d0826f884beaacca7ab42e108e
Ali Roohi <Aliroohi1306@gmail.com>
2024-11-01 14:56:45 +0330

    implementing services

--- cloud_practice2/API_app/views.py
+++ cloud_practice2/API_app/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 
 def dictionary_definition(request,word):
     print(word)
-    return HttpResponse(word)
+
+    api_url = 'https://api.api-ninjas.com/v1/dictionary?word={}'.format(word)
+    response = requests.get(api_url, headers={'X-Api-Key': 'UQNmRzC9t1DfaansEEfz1Q==04WUlN7tuwStTX9f'})
+    if response.status_code == requests.codes.ok:
+        return HttpResponse(response.text)
+        
+    else:
+        print("Error:", response.status_code, response.text)
+
+

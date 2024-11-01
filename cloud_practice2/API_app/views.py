5ab5b4185c1119a8afed7b23c7e8035e322ce44e
Ali Roohi <Aliroohi1306@gmail.com>
2024-11-01 17:11:00 +0330

    completed services

--- cloud_practice2/API_app/views.py
+++ cloud_practice2/API_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import requests
 from django.http import HttpResponse
 from django.core.cache import cache
+import json
 
 def dictionary_definition(request,word):
 
@@ -28,7 +29,17 @@ def randomword(request):
 
     api_url = 'https://api.api-ninjas.com/v1/randomword'
     response = requests.get(api_url, headers={'X-Api-Key': 'UQNmRzC9t1DfaansEEfz1Q==04WUlN7tuwStTX9f'})
+    
+
     if response.status_code == requests.codes.ok:
-        return HttpResponse(response.text)
+        parsed_data = json.loads(response.text)
+
+        word = parsed_data["word"][0]
+
+        api_url = 'https://api.api-ninjas.com/v1/dictionary?word={}'.format(word)
+        definition = requests.get(api_url, headers={'X-Api-Key': 'UQNmRzC9t1DfaansEEfz1Q==04WUlN7tuwStTX9f'})
+        cache.set(word,definition.text,timeout=300) 
+
+        return HttpResponse(f"the random word is  : {word} ----------definition: {definition.text}")
     else:
         print("Error:", response.status_code, response.text)
